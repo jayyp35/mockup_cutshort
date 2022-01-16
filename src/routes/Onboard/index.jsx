8f8f8be4371f5530d6217ecddcc78add7e1afbea
jayyp35-2 <jay@classplus.co>
2022-01-16 14:24:52 +0530

    --

--- src/routes/Onboard/index.jsx
+++ src/routes/Onboard/index.jsx
@@ -64,7 +64,6 @@ function Onboard() {
                 ? 
                     <>
                         <Loader/> 
-                        {/* Loading... */}
                     </>
                     
                 :  
